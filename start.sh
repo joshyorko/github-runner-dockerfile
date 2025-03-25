262c49c29c96c1643ccecb926a15b7399ff58bbc
jyorko <joshua.yorko@gainwelltechnolgies.com>
2025-03-25 19:31:02 +0000

    feat: :sparkles: simplify runner configuration command in start.sh

--- start.sh
+++ start.sh
@@ -12,14 +12,16 @@ cd /home/docker/actions-runner
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 echo "REGISTRATION_TOKEN ${REG_TOKEN}"
 
-./config.sh --unattended \
-  --url "https://github.com/${REPOSITORY}" \
-  --token "${REG_TOKEN}" \
-  --runnergroup "Default" \
-  --name "devpod-default-gi-e7bcd" \
-  --labels "self-hosted,self-hosted-test" \
-  --work "_work" \
-  --replace
+#./config.sh --unattended \
+#  --url "https://github.com/${REPOSITORY}" \
+#  --token "${REG_TOKEN}" \
+#  --runnergroup "Default" \
+#  --name "devpod-default-gi-e7bcd" \
+#  --labels "self-hosted,self-hosted-test" \
+#  --work "_work" \
+#  --replace
+
+./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
